8960cd0f31075514f43964429fd8071ab2e74171
Tim Kerstiens <tim.kerstiens.tk@gmail.com>
2017-09-13 14:53:49 -0600

    Feat: Add initial layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Digital Bookshelf</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="/css/normalize.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <header>
+            <h1>Digital Bookshelf</h1>
+        </header>
+        <section id="main">
+            @yield('content')
+        </section>
+    </body>
+</html>
