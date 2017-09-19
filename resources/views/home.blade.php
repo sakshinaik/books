19587927432623eb23f107be20f43785a5cc1f92
TKerstiens <tim.kerstiens.tk@gmail.com>
2017-09-19 10:41:08 -0600

    fix: Update index layout for 0 books

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,41 +6,42 @@
 @endpush
 
 @section('content')
+
 @if (count($books) === 0)
-    <section id="introduction">
+    <section id="introduction" class="center">
         <h2>You don't have any books!</h2>
         <p>Click <a href="{{ route('book.add') }}">here</a> to get started.</p>
     </section>
+@else
+    <form class="center">
+        <fieldset class="flex-container">
+            <legend>
+                Sort Book List
+            </legend>
+
+            <div class="flex-container full-width flex-item">
+                <label for="sort-field" class="input-label">Field</label>
+                <select id="sort-field" class="input-field">
+                    <option value="sort_order">Sort Order</option>
+                    <option value="author">Author</option>
+                    <option value="title">Title</option>
+                    <option value="publication_date">Publication Date</option>
+                    <option value="isbn">ISBN</option>
+                </select>
+            </div>
+
+            <div class="flex-container full-width flex-item">
+                <label for="sort-direction" class="input-label">Direction</label>
+                <select id="sort-direction" class="input-field">
+                    <option value="asc">A -> Z</option>
+                    <option value="desc">Z -> A</option>
+                </select>
+            </div>
+
+            <button class="flex-item" onclick="event.preventDefault(); sortIndex();">Sort</button>
+        </fieldset>
+    </form>
 @endif
-
-<form class="center">
-    <fieldset class="flex-container">
-        <legend>
-            Sort Book List
-        </legend>
-
-        <div class="flex-container full-width flex-item">
-            <label for="sort-field" class="input-label">Field</label>
-            <select id="sort-field" class="input-field">
-                <option value="sort_order">Sort Order</option>
-                <option value="author">Author</option>
-                <option value="title">Title</option>
-                <option value="publication_date">Publication Date</option>
-                <option value="isbn">ISBN</option>
-            </select>
-        </div>
-
-        <div class="flex-container full-width flex-item">
-            <label for="sort-direction" class="input-label">Direction</label>
-            <select id="sort-direction" class="input-field">
-                <option value="asc">A -> Z</option>
-                <option value="desc">Z -> A</option>
-            </select>
-        </div>
-
-        <button class="flex-item" onclick="event.preventDefault(); sortIndex();">Sort</button>
-    </fieldset>
-</form>
 
 <section id="book-list">
     @include('book.index')
